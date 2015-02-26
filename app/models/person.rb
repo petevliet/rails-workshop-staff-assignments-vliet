808bcae4a9b07eca8359c78b90e61c693f705afe
Peter Vliet <petervliet@gmail.com>
2015-02-26 10:43:41 -0700

    user must enter title/last or first/last for people

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,8 @@
 class Person < ActiveRecord::Base
 
+  validates :last_name, presence: true
+  validates :title || :first_name, presence: true
+
   def full_name
     "#{title} #{first_name} #{last_name}"
   end
