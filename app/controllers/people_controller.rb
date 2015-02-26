bd9b6602e66084a7dc311a812f4599fc8d1d6bf0
Peter Vliet <petervliet@gmail.com>
2015-02-26 10:19:19 -0700

    user can see staff show page

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -4,6 +4,10 @@ class PeopleController < ApplicationController
     @people = Person.all
   end
 
+  def show
+    @person = Person.find(params[:id])
+  end
+
   def new
     @person = Person.new
   end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,7 @@
 class Person < ActiveRecord::Base
 
-
+  def full_name
+    "#{title} #{first_name} #{last_name}"
+  end
 
 end
