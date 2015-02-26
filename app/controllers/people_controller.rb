e2d2fa6eaa84909660acbf35ef99c5b739102abc
Peter Vliet <petervliet@gmail.com>
2015-02-26 10:28:57 -0700

    user can edit person from show page, create form partial

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -13,12 +13,29 @@ class PeopleController < ApplicationController
   end
 
   def create
-    @person = Person.new(params.require(:person).permit(:first_name, :last_name, :title))
+    @person = Person.new(person_params)
     if @person.save
       redirect_to root_path, notice: "Person was created!"
     else
       render :new
     end
+  end
+
+  def edit
+    @person = Person.find(params[:id])
+  end
+
+  def update
+    @person = Person.find(params[:id])
+    if @person.update(person_params)
+      redirect_to person_path(@person), notice: "Person updated. congrats"
+    else
+      render :new
+    end
+  end
+
+  def person_params
+    params.require(:person).permit(:first_name, :last_name, :title)
   end
 
 end
